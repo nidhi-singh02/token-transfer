8d26cbbc868fad7fbd9a2db5b47b2e21f5a6a7ba
Avi-Gupta1 <47304452+Avi-Gupta1@users.noreply.github.com>
2023-01-05 23:22:06 +0530

    Update Header.js

--- web-app/src/components/Header/Header.js
+++ web-app/src/components/Header/Header.js
@@ -25,8 +25,9 @@ function Header(props) {
     }
     return (
         <nav className="navbar navbar-dark bg-primary">
-            <div className="row col-12 d-flex justify-content-center text-white">
+            <div className="row col-12 d-flex justify-content-left text-white">
                 <span className="h3">{props.title || title}</span>
+
                 {renderLogout()}
             </div>
         </nav>
